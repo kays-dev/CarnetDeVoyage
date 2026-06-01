01a2b6d3e1eb99df75278a94c8ce19f3d91f09b5
kays-dev <krys.b@outlook.com>
2026-06-01 14:31:05 +0200

    Initial Commit

---
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnetDeVoyageApp.swift
+//  CarnetDeVoyage
+//
+//  Created by Apprenant174 on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
